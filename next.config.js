1f41bf78973d88608cd48a6a7e239d582c005513
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-02-25 08:05:05 +0300

    migratingmy site  to Nextjs  13

--- next.config.js
+++ next.config.js
@@ -1,30 +1,6 @@
-
-/**
- * @type {import('next').NextConfig}
- */
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
-  i18n:{
-    locales:['en'],
-    defaultLocale:'en'
-  }
 }
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})
-// module.exports = withPlugins([bundleAnalyzer], nextConfig)
 
-module.exports = () => {
-  const plugins = [bundleAnalyzer];
-  const config = plugins.reduce((acc, next) => next(acc), {
-    ...nextConfig
-  })
-
-  return config;
-}
+module.exports = nextConfig
